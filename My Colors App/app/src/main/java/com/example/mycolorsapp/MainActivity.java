641f23274da6aec37c74db93977d97eee74c368d
san7iiago <santiago.28.n@gmail.com>
2019-09-18 08:33:43 -0500

    App Colors Bars

--- My Colors App/app/src/main/java/com/example/mycolorsapp/MainActivity.java
+++ My Colors App/app/src/main/java/com/example/mycolorsapp/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.mycolorsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    //Instance id's from XML form design
+    private SeekBar sbrRed = null;
+    private SeekBar sbrGreen = null;
+    private SeekBar sbrBlue = null;
+    private SeekBar sbrAlpha = null;
+    private View vieColors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the id's from xml form design
+        sbrRed = findViewById(R.id.sbrRed);
+        sbrGreen = findViewById(R.id.sbrGreen);
+        sbrBlue = findViewById(R.id.sbrBlue);
+        sbrAlpha = findViewById(R.id.sbrAlpha);
+        vieColors = findViewById(R.id.vieColors);
+
+        //Set Seekbar Change on moving
+        sbrRed.setOnSeekBarChangeListener(this);
+        sbrGreen.setOnSeekBarChangeListener(this);
+        sbrBlue.setOnSeekBarChangeListener(this);
+        sbrAlpha.setOnSeekBarChangeListener(this);
+
+        // Enable on context Menu on view component
+        registerForContextMenu(vieColors);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int r = sbrRed.getProgress();
+        int g = sbrGreen.getProgress();
+        int b = sbrBlue.getProgress();
+        int a = sbrAlpha.getProgress();
+
+        int color = Color.argb(a,r,g,b);
+        vieColors.setBackgroundColor(color);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
